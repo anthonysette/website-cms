671e1b45908194c529fc808ff6ef219c4fb4554e
Anthony Sette <anthony@sparc.agency>
2022-10-05 00:26:47 -0400

    Updated Docker File

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST'),
-  port: env.int('PORT'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 8080),
   app: {
     keys: env.array('APP_KEYS'),
   },
